e30ca4755062e46a31bda84a305b9fe693760235
DimaKyn <dimakislitsyn1996@gmail.com>
2022-08-04 18:18:03 +0300

    "Added the ability to play and pause music using index, thumb, and middle finger"

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -10,31 +10,50 @@ class handTracker():
         self.detectionCon = detectionCon
         self.modelComplex = modelComplexity
         self.trackCon = trackCon
+
+        # Mediapipe hands
         self.mpHands = mp.solutions.hands
+
+        # hands is a Hands object
+        # Hands(Keep tracking when no hand is found[T/F],maxNumOfHands INT,...
+        # ...,minimum detection confidence FLOAT, minimum tracking confidence FLOAT)
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex,
                                         self.detectionCon, self.trackCon)
+        # Method for drawing lines between every dot on the hand
         self.mpDraw = mp.solutions.drawing_utils
 
     def handsFinder(self, image, draw=True):
+        # Image object
         imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # "process" is a method which processes the image of the camera
         self.results = self.hands.process(imageRGB)
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
-
+                # Draw lines between landmarks
                 if draw:
                     self.mpDraw.draw_landmarks(image, handLms, self.mpHands.HAND_CONNECTIONS)
         return image
 
     def positionFinder(self, image, handNo=0, draw=True):
         lmlist = []
+        xlmlist = []
+        ylmlist = []
         if self.results.multi_hand_landmarks:
             Hand = self.results.multi_hand_landmarks[handNo]
+
+            # id = index of finger landmark
+            # Print landmark locations by pixels on the screen
             for id, lm in enumerate(Hand.landmark):
                 h, w, c = image.shape
+                # Position of center of the image
                 cx, cy = int(lm.x * w), int(lm.y * h)
+                # Lists location of each landmark on the screen
+                xlmlist.append(cx)
+                ylmlist.append(cy)
                 lmlist.append([id, cx, cy])
+                # Draw a circle on each hand landmarks
                 if draw:
                     cv2.circle(image, (cx, cy), 7, (255, 0, 255), cv2.FILLED)
 
-        return lmlist
+        return lmlist, xlmlist, ylmlist
--- HandTrackingProject.py
+++ HandTrackingProject.py
@@ -1,29 +1,76 @@
+import time
 import HandTrackingModule as htm
 import mediapipe as mp
 import cv2
-import time
+import pyautogui
+import webbrowser
+import numpy as np
 
+# url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+# webbrowser.register('chrome',
+#                     None, webbrowser.BackgroundBrowser(
+#         "C:\Program Files\Google\Chrome\Application//chrome.exe"))
+
+# Uses the i webcam
 cap = cv2.VideoCapture(0)
+
+# tracker is a hand tracker class
 tracker = htm.handTracker()
 
 # Write fps
 prevTime = 0
 currTime = 0
 
+pyautogui.press('play-pause')
+# Display image, find hands, then find their position
 while True:
-        success, image = cap.read()
-        image = tracker.handsFinder(image)
-        lmList = tracker.positionFinder(image)
-        if len(lmList) != 0:
-            print(lmList[4])
+    success, image = cap.read()
+    image = tracker.handsFinder(image)
+    lmList, xlmList, ylmList = tracker.positionFinder(image)
+    if len(lmList) != 0:
+        print(lmList[4])
+        print(lmList[8])
 
-        currTime = time.time()
-        fps = 1 / (currTime - prevTime)
-        prevTime = currTime
 
-        # Put fps counter on the screen
-        cv2.putText(image, str(int(fps)), (0, 25), cv2.FONT_HERSHEY_PLAIN, 2,
-                    (200, 100, 200), 3)
 
-        cv2.imshow("Video", image)
-        cv2.waitKey(1)
+        # Thumb
+        x1, y1 = xlmList[4], ylmList[4]
+        # Index finger coordinates
+        x2, y2 = xlmList[8], ylmList[8]
+        # Middle finger coordinates
+        x3, y3 = xlmList[12], ylmList[12]
+        # Ring finger coordinates
+        x4, y4 = xlmList[16], ylmList[16]
+        # Ring finger coordinates at first bend from the top
+        x4_2, y4_2 = xlmList[16], ylmList[16]
+        # Little finger
+        x5, y5 = xlmList[20], ylmList[20]
+
+        print(abs(x1 - x2) < 30 & abs(y1 - y2) < 30)
+        print(abs(x3 - x2) + abs(y3 - y2) > 50)
+        print(abs(x4_2 - x5) + abs(y4_2 - y5) < 50)
+        print(abs(x4 - x3) + abs(y4 - y3) < 80)
+
+        # Play music (Insures middle finger is close to ring finger) 👌
+        if abs(x1 - x2) < 30 & abs(y1 - y2) < 30 & \
+                abs(x3 - x2) + abs(y3 - y2) > 50 \
+                & abs(x4 - x3) + abs(y4 - y3) < 80:
+            print("PLAY")
+            pyautogui.press('play')
+
+
+        # Pause Music (Insures index finger isn't close to thumb, and middle finger touches thumb)
+        if abs(x1 - x2) + abs(y1 - y2) > 50 \
+                & abs(x3 - x1) + abs(y3 - y1) < 50 \
+                & abs(x4_2 - x5) + abs(y4_2 - y5) < 50\
+                & abs(x2 - x3) + abs(y2 - y3) > 30:
+            print("PAUSE")
+            pyautogui.press('pause')
+
+
+        # Can not open url's
+        # if (xlmList[4] - xlmList[20]) < 30 & abs(ylmList[4] - ylmList[20]) < 30:
+        #     webbrowser.get('chrome').open(url)
+
+    cv2.imshow("Video", image)
+    cv2.waitKey(1)
